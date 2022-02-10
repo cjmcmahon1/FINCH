93df9cf4dbed0c56b968826d56c77018a50452d1
Chris McMahon <cjm01@bu.edu>
2022-02-10 14:20:54 -0500

    make propagate() take an input struct

--- Short_Distance_Fresnel.m
+++ Short_Distance_Fresnel.m
@@ -5,46 +5,35 @@
 addpath('./MATLAB_functions/'); %include helper functions
 
 % Parameters; units mm
-bench_parameters = struct();
-bench_parameters.L = 250e-3;      %side length of input image
-bemch_parameters.lambda = 490e-6; %wavelength
-bench_parameters.M = 1024;        %samples
-bench_parameters.NA = 0.1;        %numerical aperture
-L = 250e-3; lambda = 490e-6; k = 2*pi/lambda; R = 5e-3;
-M = 1024; % samples
-NA = 0.1;
+PARAMS = struct;
+PARAMS.L = 250e-3;      %side length of input image
+PARAMS.lambda = 490e-6; %wavelength
+PARAMS.M = 1024;        %samples
+PARAMS.NA = 0.1;        %numerical aperture
 
-% Define spatial axes
-dx = L/M;
-x = -L/2:dx:L/2-dx;
+% Define spatial axes (unused)
+dx = PARAMS.L/PARAMS.M;
+x = -PARAMS.L/2:dx:PARAMS.L/2-dx;
 y = x;
 [X,Y] = meshgrid(x,y);
 
-% Define frequency axes
+% Define frequency axes (unused)
 fMax = 1/(2*dx);
-df = 1/L;
+df = 1/PARAMS.L;
 fx = -fMax:df:fMax-df;
 fy=fx;
 [FX,FY] = meshgrid(fx,fy);
 
-%run check that gaussian beam area doubles when we propagate
-%by the Rayleigh distance.
-%gaussian_beam_test()
-
-%run check that first bessel function result (Goodman 4.4.2)
-%has its first zero as predicted by the bessel function on
-%Wikipedia.
-%bessel_function_test()
-
 %Generate fields by Fresnel propagating constant amplitude,
 %circular aperture fields two different distances z1 & z2. 
-%using propagate(NA, z)
-p1 = propagate(0.1, 400e-3, L, M, lambda);
-p2 = propagate(0.1, 415e-3, L, M, lambda);
+%using propagate(z, parameters)
+p1 = propagate(400e-3, PARAMS);
+p2 = propagate(415e-3, PARAMS);
+%add the two fields together
 interference = struct('field', p1.field + p2.field, 'x', p1.x, 'y', p1.y);
 hfig = figure;
 pos = get(hfig,'position');
-set(hfig,'position',pos.*[.5 1 3 1]);
+set(hfig,'position',pos.*[.5 1 3 1]); %make plot window wider
 subplot(1,3,1)
 plot_im(p1, "P1 (z=200um)")
 subplot(1,3,2)
@@ -52,6 +41,18 @@ plot_im(p2, "P2 (z=220um)")
 subplot(1,3,3)
 plot_im(interference, "P1 + P2")
 
+%Other sanity checks that our Fresnel propagator works correctly.
+
+%Check that gaussian beam area doubles when we propagate
+%by the Rayleigh distance.
+%gaussian_beam_test()
+
+%Check that first bessel function result (Goodman 4.4.2)
+%has its first zero as predicted by the bessel function on
+%Wikipedia.
+%bessel_function_test()
+
+%Function Definitions
 function a = rect(x)
     a = abs(x) <= .5;
 end
@@ -67,20 +68,17 @@ function a = circularAperture(L, R, M, xC, yC)
     a = rect(.5 * ((X-xC).^2 + (Y-yC).^2) / (R^2));
 end
 
-function plane_struct = propagate(na, zf, L, M, lambda)
+function plane_struct = propagate(zf, bench_params)
     arguments
-        na              %numerical aperture
-        zf              %distance from focus (mm)
-        L = 250e-3      %length scale of input image (mm)
-        M = 1024        %number of samples
-        lambda = 490e-6 %wavelength (mm)
+        zf %distance from focus (mm)
+        bench_params
     end
     % Define frequency axes
-    dx = L/M;
-    x = -L/2:dx:L/2-dx;
+    dx = bench_params.L/bench_params.M;
+    x = -bench_params.L/2:dx:bench_params.L/2-dx;
     y = x;
     fMax = 1/(2*dx);
-    df = 1/L;
+    df = 1/bench_params.L;
     fx = -fMax:df:fMax-df;
     fy=fx;
     [FX,FY] = meshgrid(fx,fy);
@@ -88,9 +86,11 @@ function plane_struct = propagate(na, zf, L, M, lambda)
     %plane wave, the fourier transform of the field is just a circ()
     %function with radius NA/lambda (Goodman 6.2.2):
     %This should probably be normalized in some way
-    fq_aperture = (FY.^2 + FX.^2) < (na/lambda)^2;
+    cutoff_freq = bench_params.NA / bench_params.lambda;
+    fq_aperture = (FY.^2 + FX.^2) < (cutoff_freq^2);
     %The Fresnel propagator is:
-    H = fresnel_propagator(zf, L, M, lambda);
+    H = fresnel_propagator(zf, bench_params.L, ...
+                           bench_params.M, bench_params.lambda);
     %To propagate, we just multiply
     proppedFt = fftshift(fq_aperture .* H);
     plane = ifftshift(ifft2(proppedFt));
