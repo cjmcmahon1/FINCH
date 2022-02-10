defd2d0078908eedee1a05496b003b0a98495a32
Chris McMahon <cjm01@bu.edu>
2022-02-10 14:22:43 -0500

    add circularAperture.m

--- Short_Distance_Fresnel.m
+++ Short_Distance_Fresnel.m
@@ -53,21 +53,6 @@ plot_im(interference, "P1 + P2")
 %bessel_function_test()
 
 %Function Definitions
-function a = rect(x)
-    a = abs(x) <= .5;
-end
-
-function a = circularAperture(L, R, M, xC, yC)
-    % L source plane length (m)
-    % R beam radius (m)
-    % M samples
-    dx = L/M;
-    x = -L/2:dx:L/2-dx;
-    y = x;
-    [X,Y] = meshgrid(x,y);
-    a = rect(.5 * ((X-xC).^2 + (Y-yC).^2) / (R^2));
-end
-
 function plane_struct = propagate(zf, bench_params)
     arguments
         zf %distance from focus (mm)
