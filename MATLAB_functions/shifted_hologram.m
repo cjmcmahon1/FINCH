ab69fcf9dbba3a8eeb4e079b31d6da4544f44c77
Chris McMahon <cjm01@bu.edu>
2022-03-15 10:23:25 -0400

    remove pupil_func

--- MATLAB_functions/shifted_hologram.m
+++ MATLAB_functions/shifted_hologram.m
@@ -8,20 +8,5 @@ function result = shifted_hologram(plane, theta)
 %         theta %artificial phase shift of the interference
 %     end
     intensity = abs(plane.field).^2 .* exp(1i * theta);
-    %intensity = P .* (2 + h1 + h2); % MS - why add 2? also, what is P doing?
     result = struct('intensity', intensity, 'x', plane.x, 'y', plane.y);
-end
-
-function plane = pupil_func(radius, bench_params)
-    %pupil function in real space
-%     arguments
-%         radius %mm
-%         bench_params
-%     end
-    dx = bench_params.Lx/bench_params.Mx;
-    x = -bench_params.Lx/2:dx:bench_params.Lx/2-dx;
-    dy = bench_params.Ly/bench_params.My;
-    y = -bench_params.Ly/2:dy:bench_params.Ly/2-dy;
-    [X,Y] = meshgrid(x,y);
-    plane = (X.^2 + Y.^2) <= radius^2;
 end
