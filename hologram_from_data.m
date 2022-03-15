dedacff35201af731820be41bff6473c62158f6b
Chris McMahon <cjm01@bu.edu>
2022-03-15 14:37:56 -0400

    add propagation in hologram_from_data

--- hologram_from_data.m
+++ hologram_from_data.m
@@ -17,6 +17,7 @@ im3 = open_im(strcat(base_folder, '5um-im6-240deg.png'));
 h1 = image_data_struct(im1(460:660, 710:910), 0);
 h2 = image_data_struct(im2(460:660, 710:910), 2*pi/3);
 h3 = image_data_struct(im3(460:660, 710:910), 4*pi/3);
+PARAMS = bench_params(201, 201);
 % subplot(1, 3, 1)
 % plot_im(h1, 'Image 1: \theta = 0');
 % subplot(1, 3, 2)
@@ -24,7 +25,24 @@ h3 = image_data_struct(im3(460:660, 710:910), 4*pi/3);
 % subplot(1, 3, 3)
 % plot_im(h3, 'Image 3: \theta = 4\pi/3');
 c_hol = hol_from_data([h1 h2 h3]);
-plot_im(c_hol, "Complex Hologram");
+% plot_im(c_hol, "Complex Hologram");
+hfig = figure;
+pos = get(hfig,'position');
+set(hfig,'position',pos.*[0.25 0.8 2.5 1.2]); %make plot window wider
+%z_vals = linspace(3.8, 4.2, 4);
+z_vals = [-5 -4.05 0 4.05 5];
+for i = 1:length(z_vals)
+    hol = gen_hol_im(c_hol, z_vals(i), PARAMS);
+    subplot(1, length(z_vals), i);
+    sys = gen_hol_im(hol, z_vals(i), PARAMS);
+    label = sprintf('z=%3d um)', z_vals(i)*1e3);
+    plot_im(sys, label);
+end
+
+function hol_struct = gen_hol_im(hol, z, bench_params)
+    hol_plane = fresnel_prop(hol.intensity, z, bench_params);
+    hol_struct = struct('intensity', hol_plane, 'x', hol.x, 'y', hol.y);
+end
 
 function c_hologram = hol_from_data(image_struct_list)
     num_h = length(image_struct_list);
