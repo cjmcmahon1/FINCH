08432d041b977a5c206c4178bb0666cb7f71bba7
Chris McMahon <cjm01@bu.edu>
2022-03-18 10:40:11 -0400

    add 'separation' variable

--- Hologram_FT.m
+++ Hologram_FT.m
@@ -18,45 +18,54 @@ PARAMS.NA = 0.1;        %numerical aperture
 %circular aperture fields two different distances z1 & z2. 
 %using propagate(z, parameters)
 %the Brooker papers have z1~-10mm, z2~10mm
-z1 = -1; %mm
-z2 = 1; %mm
+separation = 2; %mm
+z1 = -separation/2; %mm
+z2 = separation/2; %mm
 p1 = propagate_init(z1, PARAMS);
 p2 = propagate_init(z2, PARAMS);
 %generate the complex-valued hologram
 hol = complex_hologram(p1, p2, 3);
-
 % make a 3D hologram by Fresnel propagating various z distances
-z_vals = linspace(-0.75, -0.25, 100);
+z_vals = linspace(z1/2 - separation/8, z1/2 + separation/8, 100);
 num_z_vals = size(z_vals);
 num_z_vals = num_z_vals(2);
 % propagate in the xz plane to speed up the calculation of 3D PSFs
 z_propped = fresnel_prop_xz(hol, midpt, z_vals, PARAMS);
-subplot(1, 3, 1)
+quick_ft = abs(fftshift(fft2(z_propped)));
+%full 3D PSF calculation for comparsion
+hol3d = hologram3D(hol, z_vals, PARAMS);
+hol3d_xz_im = squeeze(abs(hol3d.intensity(:,midpt,:)));
+hol3d_ft = FT(hol3d);
+hol3d_ft_im = squeeze(abs(hol3d_ft.intensity(:,midpt,:)));
+%plot the comparison
+subplot(1, 4, 1)
 imagesc(z_vals, hol.x, abs(z_propped));
 colormap('gray');
 xlabel('z (mm)');
 ylabel('x (mm)');
 title('Quick 3D PSF');
 colorbar();
+subplot(1, 4, 2);
+imagesc(hol3d_ft.fz, hol3d_ft.fx, quick_ft);
+xlabel('f_z (mm^{-1})');
+ylabel('f_x (mm^{-1})');
+title ('FT of Quick 3D PSF');
+colorbar();
 %for loop method to generate a 3d hologram
-hol3d = hologram3D(hol, z_vals, PARAMS);
-hol3d_xz_im = squeeze(abs(hol3d.intensity(:,midpt,:)));
-subplot(1, 3, 2);
+subplot(1, 4, 3);
 imagesc(hol3d.z, hol3d.x, hol3d_xz_im);
 colormap('gray');
 xlabel('z (mm)');
 ylabel('x (mm)');
 title('Full 3D PSF');
 colorbar();
-hol3d_ft = FT(hol3d);
-hol3d_ft_im = squeeze(abs(hol3d_ft.intensity(:,midpt,:)));
-subplot(1, 3, 3);
+subplot(1, 4, 4);
 colormap('gray');
 imagesc(hol3d_ft.fz, hol3d_ft.fx, hol3d_ft_im);
 xlabel('f_z (mm^{-1})');
 ylabel('f_x (mm^{-1})');
 title ('FT of Full 3D PSF');
-
+colorbar();
 function H = fresnel_propagator_xz(z_values, yslice_idx, bench_params)
     arguments
         z_values % propagataion distance
