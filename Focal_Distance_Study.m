c7406f0b2b2bbd8600d71a27ec685561e1bc3eaf
Chris McMahon <cjm01@bu.edu>
2022-05-26 15:44:38 -0400

    start adding noise

--- Focal_Distance_Study.m
+++ Focal_Distance_Study.m
@@ -34,54 +34,61 @@ fy=fx;
 %circular aperture fields two different distances z1 & z2. 
 %using propagate(z, parameters)
 num_z_vals = 10;
+noise = 1e3;
 midpt = round(PARAMS.Mx / 2);
-z_values = linspace(0, 4, num_z_vals);
+z_values = linspace(0, 1, num_z_vals);
 propagated = zeros(PARAMS.Mx, PARAMS.My, num_z_vals);
 c_hol = zeros(PARAMS.Mx, PARAMS.My, num_z_vals);
+c_hol_noisy = zeros(PARAMS.Mx, PARAMS.My, num_z_vals);
 for z_idx = 1:num_z_vals
     %for each z value, compute the initial propagated fields
     tmp_propagated = propagate_init(z_values(z_idx), PARAMS);
     propagated(:,:,z_idx) = tmp_propagated.field;
-    tmp_hol = cHol(z_values(z_idx), PARAMS);
+    tmp_hol = cHol(z_values(z_idx), 0, PARAMS);
     c_hol(:,:,z_idx)  = tmp_hol ./ sum(abs(tmp_hol), 'all'); %normalize
+    tmp_hol_noisy = cHol(z_values(z_idx), noise, PARAMS);
+    noisy_norm = sum(abs(tmp_hol_noisy), 'all');
+    c_hol_noisy(:,:,z_idx)  = tmp_hol_noisy ./ noisy_norm; %normalize
 end
 %Here, we can note that if we put the imaging plane in the exact
 %center of two point sources, the effective interference pattern is as if
 %we just took the square of one of the fields. Therefore, our
 %computationally efficient method for getting the interference pattern is
 %to just square the single propagated field at each distance. 
-int_patterns = propagated.^2;
+% int_patterns = propagated.^2;
+int_patterns = c_hol;
 int_patterns = int_patterns ./ sum(abs(int_patterns), [1 2]); %normalize
 ft_interference = fftshift(fft(int_patterns, [], 1), 1);
 figure('Name', 'MTF');
 legend_list = [];
-for z_idx =[1 5 10]
+for z_idx =[1 2 3]
     plt_vals = abs(ft_interference(:,midpt, z_idx));
-    %plot(fx, plt_vals./max(plt_vals)); %normalize
-    plot(fx, plt_vals); 
-    legend_list(end+1) = string(sprintf('z=%.2d', z_values(z_idx)));
+    plot(fx, plt_vals./max(plt_vals)); %normalize
+    %plot(fx, plt_vals);
+    legend_list = [legend_list string(sprintf('z=%.2d', z_values(z_idx)))];
     hold on;
 end
+legend(legend_list);
 title('Normalized FT of Interference Pattern');
 hold off;
-figure('Name', 'Movie');
-hol3d_ft = fftshift(fft2(propagated.^2));
-hol3d = struct('intensity', hol3d_ft, 'x', x, 'y', y, 'z', z_values);
-hol3d_frames = hologram3D_to_frames(hol3d, "Interference Pattern for " + ...
-    "Various Propagation Distances");
-movie(hol3d_frames);
+% figure('Name', 'Movie');
+% hol3d_ft = fftshift(fft2(propagated.^2));
+% hol3d = struct('intensity', hol3d_ft, 'x', x, 'y', y, 'z', z_values);
+% hol3d_frames = hologram3D_to_frames(hol3d, "Interference Pattern for " + ...
+%     "Various Propagation Distances");
+% movie(hol3d_frames);
 
 %Sanity checks that our Fresnel propagator works correctly are in
 %./Test_Scripts/
 
 %Function Definitions are in ./MATLAB_FUNCTIONS/
 
-function PSH_intensity = cHol(z, PARAMS)
+function PSH_intensity = cHol(z, noise, PARAMS)
     %generate a complex hologram from an imaging plane a distance z from
     %two bandlimited point sources. Imaging plane is assumed to be
     %perfectly centered
     p1 = propagate_init(z, PARAMS);
     p2 = propagate_init(-z, PARAMS);
-    PSH = complex_hologram(p1, p2, 3, 0, true);
+    PSH = complex_hologram(p1, p2, 3, noise, true);
     PSH_intensity = PSH.intensity;
 end
